b802d5793f28a3a8cb897655eb4cb003da465618
jiac <jiac@xuanwuai.com>
2022-07-14 17:57:49 +0800

    add java18 413

--- src/main/java/com/jcohy/sample/java18/snippets/SnippetsTest.java
+++ src/main/java/com/jcohy/sample/java18/snippets/SnippetsTest.java
@@ -1,5 +1,7 @@
 package com.jcohy.sample.java18.snippets;
 
+import java.util.Optional;
+
 /**
  * 描述: .
  * <p>
@@ -11,6 +13,7 @@ package com.jcohy.sample.java18.snippets;
  */
 public class SnippetsTest {
 
+    // tag::snippet[]
     /**
      * The following code shows how to use {@code Optional.isPresent}:
      * {@snippet :
@@ -19,7 +22,110 @@ public class SnippetsTest {
      * }
      * }
      */
-    public static void main(String[] args) {
-
+    public void snippet(Optional<String> v) {
+        if (v.isPresent()) {
+            System.out.println("v: " + v.get());
+        }
     }
+    // end::snippet[]
+
+    // tag::snippet2[]
+    /**
+     * The following code shows how to use {@code Optional.isPresent}:
+     * {@snippet file="ShowOptional.java" region="example"}
+     */
+    public void show(Optional<String> v) {
+        // @start region="example"
+        if (v.isPresent()) {
+            System.out.println("v: " + v.get());
+        }
+        // @end
+    }
+    // end::snippet2[]
+
+
+    // tag::snippet3[]
+    /**
+     * A simple program.
+     * {@snippet :
+     * class HelloWorld {
+     *     public static void main(String... args) {
+     *         System.out.println("Hello World!");      // @highlight substring="println"
+     *     }
+     * }
+     * }
+     */
+    public void show3(Optional<String> v) {
+    }
+    // end::snippet3[]
+
+    // tag::snippet4[]
+    /**
+     * {@snippet :
+     *   public static void main(String... args) {
+     *       for (var arg : args) {                 // @highlight region regex = "\barg\b"
+     *           if (!arg.isBlank()) {
+     *               System.out.println(arg);
+     *           }
+     *       }                                      // @end
+     *   }
+     *   }
+     */
+    public void show4(Optional<String> v) {
+    }
+    // end::snippet4[]
+
+    // tag::snippet5[]
+    /**
+     * {@snippet :
+     *   public static void main(String... args) {
+     *       for (var arg : args) {                 // @highlight region regex = "\barg\b"
+     *           if (!arg.isBlank()) {
+     *               System.out.println(arg);
+     *           }
+     *       }                                      // @end
+     *   }
+     *   }
+     */
+    public void show5(Optional<String> v) {
+    }
+    // end::snippet5[]
+
+    // tag::snippet6[]
+    /**
+     * A simple program.
+     * {@snippet :
+     * class HelloWorld {
+     *     public static void main(String... args) {
+     *         System.out.println("Hello World!");  // @link substring="System.out" target="System#out"
+     *     }
+     * }
+     * }
+     */
+    public void show6(Optional<String> v) {
+    }
+    // end::snippet6[]
+
+    // tag::snippet7[]
+    /**
+     * Here are the configuration properties:
+     * {@snippet file="config.properties"}
+     */
+    public void show7(Optional<String> v) {
+    }
+    // end::snippet7[]
+
+    // tag::snippet8[]
+    /**
+     * Here are some example properties:
+     * {@snippet lang=properties :
+     *   local.timezone=PST
+     *   # @highlight regex="[0-9]+" :
+     *   local.zip=94123
+     *   local.area-code=415
+     *   }
+     */
+    public void show8(Optional<String> v) {
+    }
+    // end::snippet8[]
 }
