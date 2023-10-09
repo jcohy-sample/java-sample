0c38619020759e6c410055722d215aba56a8eb29
Jcohy <jia_chao23@126.com>
2023-10-09 15:41:10 +0800

    变更

---
+++ src/main/java/com/jcohy/sample/juc/SequentialExecutionWithJoin.java
@@ -0,0 +1,86 @@
+package com.jcohy.sample.juc;
+
+/**
+ * Copyright: Copyright (c) 2023 <a href="https://www.jcohy.com" target="_blank">jcohy.com</a>
+ *
+ * <p> Description: 线程顺序执行，使用 join
+ *
+ * @author jiac
+ * @version 2023.0.1 2023/10/9:10:28
+ * @since 2023.0.1
+ */
+public class SequentialExecutionWithJoin {
+    public static void main(String[] args) {
+        var thread1 = new Thread(new Worker(),"Thread1");
+        var thread2 = new Thread(new Worker(),"Thread2");
+        var thread3 = new Thread(new Worker(),"Thread3");
+
+
+        try {
+            // 启动第一个线程
+            thread1.start();
+            // 等待第一个线程执行完毕
+            thread1.join();
+
+            // 启动第二个线程
+            thread2.start();
+            // 等待第二个线程执行完毕
+            thread2.join();
+
+            // 启动第三个线程
+            thread3.start();
+            // 等待第三个线程执行完毕
+            thread3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+//        var thread1 = new Thread(() -> {
+//            try {
+//                String threadName = Thread.currentThread().getName();
+//                System.out.println("Thread " + threadName + " is running");
+//                Thread.sleep(1000);
+//                System.out.println("Thread " + threadName + " is finished");
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        },"Thread1");
+//        var thread2 = new Thread(() -> {
+//            try {
+//                thread1.join();
+//                String threadName = Thread.currentThread().getName();
+//                System.out.println("Thread " + threadName + " is running");
+//                Thread.sleep(1000);
+//                System.out.println("Thread " + threadName + " is finished");
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        },"Thread2");
+//        var thread3 = new Thread(() -> {
+//            try {
+//                thread2.join();
+//                String threadName = Thread.currentThread().getName();
+//                System.out.println("Thread " + threadName + " is running");
+//                Thread.sleep(1000);
+//                System.out.println("Thread " + threadName + " is finished");
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        },"Thread3");
+    }
+
+
+    static class Worker implements Runnable {
+        @Override
+        public void run() {
+            try {
+                String threadName = Thread.currentThread().getName();
+                System.out.println("Thread " + threadName + " is running");
+                Thread.sleep(1000);
+                System.out.println("Thread " + threadName + " is finished");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/jcohy/sample/juc/SequentialExecutionWithLock.java
@@ -0,0 +1,74 @@
+package com.jcohy.sample.juc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Copyright: Copyright (c) 2023 <a href="https://www.jcohy.com" target="_blank">jcohy.com</a>
+ *
+ * <p> Description:
+ *
+ * @author jiac
+ * @version 2023.0.1 2023/10/9:11:00
+ * @since 2023.0.1
+ */
+public class SequentialExecutionWithLock {
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        var condition1 = lock.newCondition();
+        var condition2 = lock.newCondition();
+        var condition3 = lock.newCondition();
+
+        var thread1 = new Thread(new Worker(lock,condition1,condition2),"Thread 1");
+        var thread2 = new Thread(new Worker(lock,condition2,condition3),"Thread 2");
+        var thread3 = new Thread(new Worker(lock,condition3,condition1),"Thread 3");
+
+
+        try {
+            // 启动所有线程
+            thread1.start();
+            thread2.start();
+            thread3.start();
+
+            // 唤醒第一个线程
+            lock.lock();
+            condition1.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    static class Worker implements Runnable {
+        private final Lock lock;
+
+        private final Condition currentCondition;
+
+        private final Condition nextCondition;
+
+        public Worker(Lock lock, Condition currentCondition, Condition nextCondition) {
+            this.lock = lock;
+            this.currentCondition = currentCondition;
+            this.nextCondition = nextCondition;
+        }
+
+        @Override
+        public void run() {
+            String threadName = Thread.currentThread().getName();
+            lock.lock();
+            try {
+                // 等待条件满足
+                currentCondition.await();
+                System.out.println("Thread " + threadName + " is running");
+                Thread.sleep(1000);
+                System.out.println("Thread " + threadName + " is finished");
+                nextCondition.signal();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
